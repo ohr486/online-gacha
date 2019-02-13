bfdfde206b996f72a516c041cdfb1eb6e2edbb05
ohr486 <ohr486@gmail.com>
2019-02-13 21:42:19 +0900

    update admin

--- rails/app/controllers/admin/users_controller.rb
+++ rails/app/controllers/admin/users_controller.rb
@@ -69,6 +69,6 @@ class Admin::UsersController < Admin::ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:provider, :uid, :user_name, :image_url)
+      params.require(:user).permit(:provider, :uid, :user_name, :user_nickname, :image_url)
     end
 end
--- rails/app/views/admin/users/_user.json.jbuilder
+++ rails/app/views/admin/users/_user.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! user, :id, :provider, :uid, :user_name, :image_url, :created_at, :updated_at
+json.extract! user, :id, :provider, :uid, :user_name, :user_nickname, :image_url, :created_at, :updated_at
 json.url user_url(user, format: :json)
